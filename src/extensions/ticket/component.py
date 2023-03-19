bbb3cd81f8c3cb46e9d45adba192aab523b4bc1b
sushi-chaaaan <mail@sushichan.live>
2023-03-19 15:47:08 +0900

    add feature: contact_mod

--- src/extensions/ticket/component.py
+++ src/extensions/ticket/component.py
@@ -1,7 +1,8 @@
 import discord
 from discord import ui
 
-from const import channel_id, custom_id
+from const import channel_id, custom_id, general
+from utils.logger import getMyLogger
 from utils.time import TimeUtils
 
 
@@ -15,8 +16,6 @@ class CreateTicketButton(ui.Button):  # type: ignore
         )
 
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.defer(ephemeral=True)
-
         # このButtonは専用コマンドでチケット用TextChannelに送信されるので
         # interaction.channelはTICKET_CHANNEL_IDのTextChannelになる
         if (
@@ -26,17 +25,32 @@ class CreateTicketButton(ui.Button):  # type: ignore
         ):
             return
 
-        ticket_channel: discord.TextChannel = interaction.channel
-        ticket_date = TimeUtils.dt_to_str(format="%m%d")
-        # TODO: Modalでチケット名を入力できるようにする
-        ticket_name = f"{ticket_date}-ModalResult"
+        modal = GetTicketNameModal()
+        await interaction.response.send_modal(modal)
+        await modal.wait()
+        if modal.failed:
+            return
 
-        await ticket_channel.create_thread(
+        ticket_date = TimeUtils.dt_to_str(format="%Y%m%d")
+        ticket_name = f"{ticket_date}_{modal.ticket_name.value}"
+        ticket_channel: discord.TextChannel = interaction.channel
+
+        mod_mention = f"<@&{general.MODERATOR_ROLE_ID}>"
+
+        thread = await ticket_channel.create_thread(
             name=ticket_name,
             message=None,
             auto_archive_duration=10080,
             type=discord.ChannelType.private_thread,
             invitable=False,
+        )
+        await thread.send(
+            f"""
+お問い合わせありがとうございます。
+このスレッドでは運営メンバーと直接話し合うことができます。
+お困りのことについて教えてください。
+{mod_mention}
+"""
         )
         return
 
@@ -44,3 +58,29 @@ class CreateTicketButton(ui.Button):  # type: ignore
 class GetTicketNameModal(ui.Modal):
     def __init__(self) -> None:
         super().__init__(title="チケット名入力", timeout=None, custom_id=custom_id.GET_TICKET_NAME_MODAL)
+
+        # TODO: placeholder
+        self.ticket_name: ui.TextInput = ui.TextInput(  # type: ignore
+            label="お問い合わせの内容を簡単に入力してください。(最大30文字)",
+            style=discord.TextStyle.short,
+            placeholder="例: どのチャンネルに投稿すればいいかわからない",
+            required=True,
+            max_length=30,
+        )
+        self.add_item(self.ticket_name)
+        self.failed = False
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        await interaction.response.defer(ephemeral=True)
+        self.stop()
+        await interaction.followup.send("入力を受け付けました。", ephemeral=True)
+        return
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:  # type: ignore
+        await interaction.response.defer(ephemeral=True)
+        self.stop()
+        self.failed = True
+        logger = getMyLogger(__name__)
+        logger.exception(error)
+        await interaction.followup.send("エラーが発生しました。", ephemeral=True)
+        return
